5b50e867f82ff40bb56c8ffbf89f0b29d595dda5
wit543 <norawit543@hotmail.com>
2015-12-14 00:18:42 +0700

    Update jump.cpp

--- jump.cpp
+++ jump.cpp
@@ -1,50 +1,55 @@
 // Example program
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+    int n,r;
+    vector<int> gaphX[101][101];
+    vector<int> gaphY[101][101];
+    int x[1000];
+    int y[1000];
+    bool visited[1000];
+void init()
+{
+    for(int i=0; i<n; i++)
+    visited[i] = false;
+}
+int search(int x,int y)
+{
+    if((100-x)*(100-x)+(100-y)*(100-y)<=r*r)
+    {
+        return 1;
+    }
+    for(int i=0;i<=n;i++){;
+        if((x-gaphX[x][y][i])*(x-gaphX[x][y][i])+(y-gaphY[x][y][i])*(y-gaphY[x][y][i])<=r*r)
+        {
+            cout<<"::"<<x<<" "<<y<<endl;
+            return search(gaphX[x][y][i],gaphY[x][y][i])+1;
+        }
+    }
+    return -1;
+}
 int main()
 {
-  int n,r;
-  cin>>n>>r;
-  int x[n];
-  int y[n];
-  int xI=0,yI=0;
-  int count=0;
-  for(int i=0;i<n;i++){
-        cin>>x[i]>>y[i];
-      }
-      for(int i=0;i<n;i++){
-          int xM=xI,yM=yI;
-          for(int j=0;j<n;j++){
-              if((x[j]-xI)*(x[j]-xI)+(y[j]-yI)*(y[j]-yI)<=r*r){
-                 
-                 if((xI-x[j])*(xI-x[j])+(yI-y[j])*(yI-y[j])>
-                 (xI-xM)*(xI-xM)+(yI-yM)*(yI-yM)&&
-                  (100-x[j])*(100-x[j])+(100-y[j])*(100-y[j])<
-                 (100-xM)*(100-xM)+(100-yM)*(100-yM)){
-                     xM=x[j];
-                     yM=y[j];
-                     x[i]=-100;
-                     y[i]=-100;
-
-                     }
-              }
-             
-          }
-          xI=xM;
-          yI=yM;
-          count++;
-          // cout<<"x: "<<xI<<"y: "<<yI<<endl;
-        if(xI!=xM&&yI!=yM){
-          count ++;}
-          if((100-xI)*(100-xI)+(100-yI)*(100-yI)<=r*r){
-            count++;
-            break;
-            }
-      }
-      if((100-xI)*(100-xI)+(100-yI)*(100-yI)>r*r){
-            count=-1;
-            }
-      cout<<count<<endl;
+    cin>>n>>r;
+    init();
+    x[0]=0;
+    y[0]=0;
+    for(int i=1;i<=n;i++)
+    {
+        int u,v;
+        scanf("%d %d",&u,&v);
+        x[i]=u;
+        y[i]=v;
+        
+        for(int j=0;j<i;j++)
+        {
+            // cout<<"::"<<i<<" "<<j<<endl;
+            gaphX[x[j]][y[j]].push_back(u);
+            gaphY[x[j]][y[j]].push_back(v);
+        }
+        // cout<<i<<endl;
+    }
+    cout<<search(0,0)<<endl;
 }
