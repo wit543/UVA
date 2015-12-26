d94944e98054bbd770e44d330769535e115e2b37
wit543 <norawit543@hotmail.com>
2015-12-27 01:35:44 +0700

    Update jump.cpp

--- jump.cpp
+++ jump.cpp
@@ -1,66 +1,90 @@
-
 // Example program
 #include <iostream>
 #include <string>
 #include <vector>
 #include <queue>
 #include <stdio.h>
-#include <set>
-#include <map>
+#include <stdlib.h>
 
 using namespace std;
-
-int main() {
-
-    int n,k;
-    cin>>n>>k;
-
-    int aci[100000],ami[100000],asi[100000],api[100000];
-    set<pair<int,int>> ci,mi,si;
-    for(int i=0;i<n;i++){
-        int ciin,miin,siin,piin;
-        cin>>ciin>>miin>>siin>>piin;
-//        scanf("%d %d %d %d",&ciin,&miin,&siin,&piin);
-        ci.insert(pair<int,int>(ciin,i));
-        mi.insert(pair<int,int>(miin,i));
-        si.insert(pair<int,int>(siin,i));
-        aci[i]=ciin;
-        ami[i]=miin;
-        asi[i]=siin;
-        api[i]=piin;
-    }
-    for(int i=0;i<k;i++){
-        int t,in,o=0;
-        bool f=false;
-        cin>>t>>in;
-//        scanf("%d %d",&t,&in);
-        if(t==1){
-            if(ci.lower_bound(pair<int,int>(in,-1))!=ci.end()){
-                o = ci.lower_bound(pair<int,int>(in,-1))->second;
-                f=true;
-            }
-
-        }
-        if(t==2){
-            if(mi.lower_bound(pair<int,int>(in,-1))!=mi.end()){
-                o = mi.lower_bound(pair<int,int>(in,-1))->second;
-                f=true;
-            }
-        }
-        if(t==3){
-            if(si.lower_bound(pair<int,int>(in,-1))!=si.end()){
-                o = si.lower_bound(pair<int,int>(in,-1))->second;
-                f=true;
-            }
-        }
-        if(t>=1&&t<=3&&f){
-            cout<<api[o]<<endl;
-            ci.erase(pair<int,int>(aci[o],o));
-            mi.erase(pair<int,int>(ami[o],o));
-            si.erase(pair<int,int>(asi[o],o));
-        }
-        if(t>=1&&t<=3&&!f){
-           cout<<0<<endl;
+vector<int> gaphX[101][101];
+vector<int> gaphY[101][101];
+int x[1000],y[1000];
+//bool xVisit[101][101],yVisit[101][101];
+int main(){
+    int n,r;
+    cin>>n>>r;
+    x[0]=0;
+    y[0]=0;
+    cout<<n<<" "<<r<<endl;
+    for(int i=1;i<=n;i++){
+        int u,v;
+        scanf("%d %d",&u,&v);
+        cout<<u<<" "<<v<<endl;
+        x[i]=u;
+        y[i]=v;
+        cout<<u<<" "<<v<<endl;
+        for(int j=0;j<i;j++)
+        {
+            gaphX[x[j]][y[j]].push_back(u);
+            gaphY[x[j]][y[j]].push_back(v);
+            gaphX[u][v].push_back(x[j]);
+            gaphY[u][v].push_back(y[j]);
         }
     }
+//    queue<int> qx,qy;
+//    qx.push(x[0]);
+//    qy.push(y[0]);
+//    xVisit[x[0]][y[0]]=true;
+//    yVisit[x[0]][y[0]]=true;
+//    int out =0;
+//    int lx=0,ly=0;
+//    bool layer = true;
+//    while(!qx.empty()&&!qy.empty()){
+//        int ix = qx.front();
+//        qx.pop();
+//        int iy = qy.front();
+//        qy.pop();
+//        if(abs((100-ix)*(100-ix))+
+//           abs((100-iy)*(100-iy))<=r*r){
+////            cout<<"o: "<<out<<endl;
+////            cout<<"go: "<<gaphX[x[ix]][y[iy]][i]<<" "<<gaphY[x[ix]][y[iy]][i]<<endl;
+//            out++;
+//            break;
+//        }
+//        if(lx==ix&&ly==iy){
+//            out++;
+////            cout<<"out: "<<ix<<" "<<iy<<endl;
+//            layer= true;
+//        }
+//        int ilx,ily;
+//        for(int i=0;i<n;i++){
+//            if(abs((gaphX[x[ix]][y[iy]][i]-ix)*(gaphX[x[ix]][y[iy]][i]-ix))+
+//                    abs((gaphY[x[ix]][y[iy]][i]-iy)*(gaphY[x[ix]][y[iy]][i]-iy))<=r*r){
+//                if(!xVisit[gaphX[x[ix]][y[iy]][i]][gaphY[x[ix]][y[iy]][i]]&&
+//                        !yVisit[gaphX[x[ix]][y[iy]][i]][gaphY[x[ix]][y[iy]][i]]){
+//                    xVisit[gaphX[x[ix]][y[iy]][i]][gaphY[x[ix]][y[iy]][i]]=true;
+//                    yVisit[gaphX[x[ix]][y[iy]][i]][gaphY[x[ix]][y[iy]][i]]=true;
+//                    qx.push(gaphX[x[ix]][y[iy]][i]);
+//                    qy.push(gaphY[x[ix]][y[iy]][i]);
+//                    ilx=gaphX[x[ix]][y[iy]][i];
+//                    ily=gaphY[x[ix]][y[iy]][i];
+////                    cout<<"g: "<<gaphX[x[ix]][y[iy]][i]<<" "<<gaphY[x[ix]][y[iy]][i]<<" "<<ix<<" "<<iy<<endl;
+//                }
+//            }
+//        }
+//        if(layer){
+//            lx=ilx;
+//            ly=ily;
+//            layer=false;
+////            cout<<"layer:  "<<lx<<" "<<ly<<endl;
+//        }
+//    }
+////    for(int i=0;i<n;i++){
+////        for(int j=0;j<n;j++) {
+////            cout << gaphX[x[i]][y[i]][j] << "," << gaphY[x[i]][y[i]][j] << " ";
+////        }
+////        cout<<endl;
+////    }
+//    cout<<out<<endl;
 }
