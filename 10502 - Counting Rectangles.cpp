dad540f5ca05d1525592c1eec5d8b0f57230ca31
wit543 <norawit543@hotmail.com>
2015-12-27 22:08:37 +0700

    Update 10502 - Counting Rectangles.cpp

--- 10502 - Counting Rectangles.cpp
+++ 10502 - Counting Rectangles.cpp
@@ -8,35 +8,43 @@
 
 using namespace std;
 int main(){
-
+   char a[100][100];
+    int n,m;
    while(true){
-       int n,m;
+
        cin>>n;
        if(n==0){break;}
        cin>>m;
-       char a[n][m];
+       if(m==0){break;}
        for(int i=0;i<n;i++)
            for(int j=0;j<m;j++)
                cin>>a[i][j];
        int out =0;
 
-       for(int i=0;i<=n;i++)
-           for(int j=0;j<=m;j++)
-               for(int k=0;k<n;k++)
-                   for(int l=0;l<m;l++) {
+       for(int k=0;k<n;k++)
+           for(int l=0;l<m;l++) {
+               if(a[k][l]!='1'){ continue;}
+               for(int i=1;i<=n;i++)
+                   for(int j=1;j<=m;j++){
+                       if (a[k][l] != '1') continue;
                        int c = 0;
+                       bool have = true;
+                       bool in = false;
                        for (int o = k; o < k+i&&o<n; o++) {
                            for (int p = l; p < l + j && p < m; p++) {
-                               if (a[o][p] == '1') {
-                                   c++;
+                               if (a[o][p] != '1') {
+                                   have = false;
+                                   o+=i;
+                                   p+=j;
                                }
                            }
                        }
-                       if(c==i*j&&c>0){
-//                           cout<<"ok"<<" "<<i<<" "<<j<<" "<<k<<" "<<l<<endl;
+
+                       if(have&&(k+i)<=n&&(l+j)<=m){
                            out++;
                        }
                    }
+           }
        cout<<out<<endl;
    }
 }
