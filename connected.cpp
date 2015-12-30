ed7c2c00ea22dda5e5096132779d2e3c2c7011d4
wit543 <norawit543@hotmail.com>
2015-12-30 23:35:33 +0700

    Create connected.cpp

---
+++ connected.cpp
@@ -0,0 +1,36 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+vector<int> gaphX[100001];
+bool xVisit[100001];
+int main(){
+    int n,r;
+    cin>>n>>r;
+    for(int i=1;i<=r;i++){
+        int u,v;
+        cin>>u>>v;
+        gaphX[u].push_back(v);
+        gaphX[v].push_back(u);
+
+    }
+    int out =0;
+    for(int i=1;i<=n;i++){
+        if(!xVisit[i]) {
+            out++;
+            xVisit[i]=true;
+            while (true) {
+                if(gaphX[i].empty())break;
+                int p = gaphX[i].back();
+                gaphX[i].pop_back();
+                xVisit[p]=true;
+            }
+        }
+    }
+    cout<<out<<endl;
+}
